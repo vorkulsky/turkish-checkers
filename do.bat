1f3899ba98a7793911df8abeb3d6779199185d7b
vorkulsky <vorkulsky@gmail.com>
2013-11-17 16:33:11 +0600

    Revert "C"
    This reverts commit e0330b4ad4d47e4a96432cf4e974296c62d2be79.

--- do.bat
+++ do.bat
@@ -1,4 +1,2 @@
-tasm /m5 /l game
-tcc -S game_c.c
-tcc -c game_c.c
-tlink /t /x game.obj game_c.obj,,,game.lib
+tasm /m5 /la game
+tlink /t /x game.obj,,,game.lib
--- game.asm
+++ game.asm
@@ -3,10 +3,21 @@
 .code
 locals
 ; com.asm
-	extrn main: near
+	extrn Ser_Ini: near
+	extrn Ser_Rst: near
+	extrn Out_Chr: near
+	extrn Get_Chr: near
+; int9h.asm
+	extrn Key_Ini: near
+	extrn Key_Rst: near
+	extrn Key_Is_Esc: near
 org 100h
 
 Prog:
-	call main
+	call Key_Ini
+	call Ser_Ini
+	
+	call Ser_Rst
+	call Key_Rst
 	ret
 end Prog
