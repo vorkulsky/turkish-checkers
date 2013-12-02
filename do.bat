3c83344c38455b8bb4cf1bb80fac66f6c2cab2a6
vorkulsky <vorkulsky@gmail.com>
2013-12-02 18:23:58 +0600

    -O2 -d

--- do.bat
+++ do.bat
@@ -2,6 +2,6 @@
 if not exist game.obj tasm /m5 /l game
 if exist gamec.asm del gamec.asm
 if exist gamec.obj del gamec.obj
-bcc -S -3 gamec.cpp
-if exist gamec.asm bcc -c -3 gamec.cpp
+bcc -S -3 -O2 -d gamec.cpp
+if exist gamec.asm bcc -c -3 -O2 -d gamec.cpp
 if exist gamec.obj tlink /t /x game.obj gamec.obj,,,game.lib
